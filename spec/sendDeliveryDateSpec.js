368ff8dcfde0d0c306407fa2459e8ef72e91bc7d
Hubert Pompecki <hpompecki@gmail.com>
2017-01-25 14:11:21 +0000

    Don't send emails to missingcustomeremail@matalandirect.com

--- spec/sendDeliveryDateSpec.js
+++ spec/sendDeliveryDateSpec.js
@@ -7,6 +7,7 @@ const deliveryDateTextEmail = require('../lib/templates/deliveryDateTextEmail.js
 
 // fixtures
 const input = require('./fixtures/sendDeliveryDateEmail/input.json')
+const inputMissingEmail = require('./fixtures/sendDeliveryDateEmail/inputMissingEmail.json')
 const emailResponse = require('./fixtures/sendDeliveryDateEmail/emailResponse.json')
 
 nock.disableNetConnect()
@@ -14,7 +15,6 @@ nock.disableNetConnect()
 console.log = () => {}
 
 describe("Sending delivery date emails", () => {
-
   beforeEach((done) => {
     this.msg = {
       body: input
@@ -50,6 +50,37 @@ describe("Sending delivery date emails", () => {
 
   it("Sends an email with a delivery date for pallet orders only", () => {
     expect(this.emailRequest.isDone()).toBe(true)
+  })
+
+  it("Emits the original message", () => {
+    let action = this.self.emit.calls.argsFor(0)[0]
+    let payload = this.self.emit.calls.argsFor(0)[1].body
+    expect(action).toEqual('data')
+    expect(payload).toEqual(this.msg.body)
+  })
+
+  it("Emits end", () => {
+    expect(this.self.emit).toHaveBeenCalledTimes(2)
+    expect(this.self.emit).toHaveBeenCalledWith('end')
+  })
+})
+
+describe("With missing email address", () => {
+  beforeEach((done) => {
+    this.msg = {
+      body: inputMissingEmail
+    }
+    this.cfg = {
+      serverToken: "servertoken",
+      postmarkHost: "https://api.postmarkapp.com",
+      from: "hello@matalandirect.com"
+    }
+    this.self = {
+      emit(action) { if (action == 'end') done(); }
+    }
+    spyOn(this.self, 'emit').and.callThrough()
+
+    sendDeliveryDateEmail.call(this.self, this.msg, this.cfg)
   })
 
   it("Emits the original message", () => {
